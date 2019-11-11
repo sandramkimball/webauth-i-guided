d65a56dd095a1d2b5f0a74add3bccf794d297ba7
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2019-11-11 09:33:25 -0800

    attempted password hashing

--- api/api-router.js
+++ api/api-router.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
@@ -9,5 +11,19 @@ router.use('/users', usersRouter);
 router.get('/', (req, res) => {
   res.json({ api: "It's alive" });
 });
+
+
+//read password from body
+//hash password
+//return to user in object
+//{password:'original pass', hash: 'hash password'}
+router.post('/hash', (req, res)=> {
+ bcrypt.genSalt(10, function (err, salt) {
+   bcrypt.hash(req.body, salt, function(err, hash){
+     //store hash in password db
+   });
+ });
+    res.json({hash})
+})
 
 module.exports = router;
